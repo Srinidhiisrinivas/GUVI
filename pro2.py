5aa0c0c52568388927c7add082bbd06959613258
Srinidhiisrinivas <51356605+Srinidhiisrinivas@users.noreply.github.com>
2019-07-24 22:47:13 +0530

    Update pro2.py

--- pro2.py
+++ pro2.py
@@ -1,6 +1,6 @@
 from itertools import combinations
-m,k=map(int,input().split())
-a=len(str(m))
-lst=list(combinations(str(m),a-k))
+a,b=map(int,input().split())
+c=len(str(a))
+lst=list(combinations(str(a),c-b))
 lst=sorted(lst)
 print(*lst[0],sep='')
