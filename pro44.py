c66ac307b74d9d197fd24ad9c7b1db3fd6ecf651
Srinidhiisrinivas <51356605+Srinidhiisrinivas@users.noreply.github.com>
2019-07-24 23:37:34 +0530

    Update pro44.py

--- pro44.py
+++ pro44.py
@@ -1,9 +1,9 @@
 ar,br,cr,d = map(int, input().split())
 xii = list(map(int, input().split()))
-z = []
+zzz = []
 i = 0
 for i in range(0, len(xii)):
     for j in range(i, len(xii)):
         for k in range(j, len(xii)):
-            z.append(sum((br*xii[i], cr*xii[j], d*xii[k])))
-print(max(z))
+            zzz.append(sum((br*xii[i], cr*xii[j], d*xii[k])))
+print(max(zzz))
